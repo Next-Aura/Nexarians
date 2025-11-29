268bcd4c464c35d80e324bc9285182b785ea7327
HioDza <randompangeran@gmail.com>
2025-11-29 20:01:57 +0700

    NexGML 1.3.0b3 (Beta 3), add 'detect.py' to 'guardians' module for clarity

--- nexgml/guardians/arr.py
+++ nexgml/guardians/arr.py
@@ -1,6 +1,5 @@
 import numpy as np
 from warnings import warn
-from scipy import sparse
 
 def safe_array(arr: np.ndarray, max_value: float=1e10, min_value: float=-1e10) -> np.ndarray:
     """Safely convert array to finite numbers within specified bounds.
@@ -30,39 +29,3 @@ def safe_array(arr: np.ndarray, max_value: float=1e10, min_value: float=-1e10) -
         warn("There's NaN or infinity value.", RuntimeWarning)
 
     return arr
-
-def hasinf(arr: np.ndarray | list | sparse.spmatrix) -> bool:
-    """Check if there's an infinity value in an array, list, or sparse matrix.
-    
-    ## Args:
-        **arr**: *np.ndarray* or *list* or *spmatrix*
-        Input array to be processed.
-
-    ## Returns:
-        **bool**: *Condition, if there's an infinity value will return True, if not then return False.*
-
-    ## Raises:
-        **None**
-    """
-    arr = np.asarray(arr)
-    stat = np.any(np.isinf(arr))
-
-    return stat
-
-def hasnan(arr: np.ndarray | list | sparse.spmatrix) -> bool:
-    """Check if there's a NaN in an array, list, or sparse matrix.
-    
-    ## Args:
-        **arr**: *np.ndarray* or *list* or *spmatrix*
-        Input array to be processed.
-
-    ## Returns:
-        **bool**: *Condition, if there's a NaN will return True, if not then return False.*
-
-    ## Raises:
-        **None**
-    """
-    arr = np.asarray(arr)
-    stat = np.any(np.isnan(arr))
-
-    return stat
